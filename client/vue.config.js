8b311ff99c67a0f3dfc6ec08625e13c1e67fab78
WangJie <605720873@163.com>
2019-06-03 14:08:37 +0800

    wj
    client

---
+++ client/vue.config.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+  outputDir : process.env.outputDir,
+  // eslint-loader 是否在保存的时候检查
+  lintOnSave: true,
+  //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+  assetsDir: 'static',
+  //生产环境是否生成 sourceMap 文件，一般情况不建议打开
+  productionSourceMap: false,
+  devServer: {
+    // host: 'localhost',
+    host: "0.0.0.0",
+    port: 8080, // 端口号
+    https: false, // https:{type:Boolean}
+    open: true, //配置自动启动浏览器
+    hotOnly: true, // 热更新
+    proxy: { //配置自动启动浏览器
+      '/api': {
+        target: 'http://waterdropapitest.aldwx.com/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/'
+        }
+      }
+
+    }
+  },
+};
--- server/src/app.js
+++ server/src/app.js
@@ -3,24 +3,18 @@ const app = new Koa();
 const {Token_Test, Token_Test_Error} = require('./middleware/token/index');
 const _router = require('./router/index');
 const bodyParser = require('koa-bodyparser');
+const formidable = require('koa-formidable'); // 图片处理
 const serve = require('koa-static');
 const path = require('path');
 const port = process.env.PORT || 5555;
 const staticPath = "../static";
 app.use(bodyParser());
-app.use(serve(
-  path.join(__dirname,staticPath)
-));
-
-console.log( path.join(__dirname,staticPath))
+app.use(serve(path.join(__dirname,staticPath)));
 
 app.use(Token_Test_Error);
 app.use(Token_Test());
 app.use(_router.routes());
 app.use(_router.allowedMethods());
-
-
-
 
 
 app.listen(port, () => {console.log(`port :${port} success`)});
